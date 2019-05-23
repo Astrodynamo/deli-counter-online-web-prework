7717eb130d71efd8e3c045f2fe49e7b4f24da5a7
Amanda King <amapickell@gmail.com>
2019-05-23 20:27:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,11 @@
 katz_deli = []
 
 def line (array)
-  
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    array.each_with_index { |name, place| puts (place + 1).to_s + ". #{name}"}
 end
 
 def take_a_number (array, name)
