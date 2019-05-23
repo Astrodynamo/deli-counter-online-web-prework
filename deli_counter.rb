dcf54ad2641d776d5f25586394937262e76473eb
Amanda King <amapickell@gmail.com>
2019-05-23 20:27:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line (array)
   else
     puts "The line is currently: "
     array.each_with_index { |name, place| puts (place + 1).to_s + ". #{name}"}
+  end
 end
 
 def take_a_number (array, name)
